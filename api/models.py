12ade2cd4aef8e0edf6a8c216ca4a0e1dea29b37
Erik Sanchez <erik.sape@gmail.com>
2020-09-06 20:47:37 -0500

    :card_file_box: added model and migration

--- api/models.py
+++ api/models.py
@@ -2,4 +2,14 @@ from django.db import models
 
 
 class Scraper(models.Model):
-    pass
+    """
+    Structure for Scrapper model
+    """
+    currency = models.CharField(max_length=50, unique=True)
+    frequency = models.IntegerField()
+    value = models.FloatField(default=0)
+    created_at = models.DateTimeField(auto_now_add=True)
+    value_updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.currency
