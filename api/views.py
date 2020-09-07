565f8ccb5173b2edd50adb20fbca20ea6b6a4947
Erik Sanchez <erik.sape@gmail.com>
2020-09-06 20:51:54 -0500

    :hammer: added api endpoints

--- api/views.py
+++ api/views.py
@@ -1,15 +1,61 @@
+import json
+
+from django.core import serializers
+from django.http import JsonResponse, HttpResponse
 from django.views.generic import View
+
+from api.forms import CreateScrapper, UpdateScrapper, DeleteScrapper
+from api.models import Scraper
 
 
 class ScraperAPI(View):
     def get(self, *args, **kwargs):
-        pass
+        data = json.loads(serializers.serialize('json', list(Scraper.objects.all())))
+        return JsonResponse({
+            'scrapers': [{'id': d['pk'], 'currency': d['fields']['currency'], 'frequency': d['fields']['frequency'],
+                          'value': d['fields']['value'], 'created_at': d['fields']['created_at'],
+                          'value_updated_at': d['fields']['value_updated_at']} for d in data]
+        }, safe=False)
 
-    def post(self, *args, **kwargs):
-        pass
+    def post(self, request, *args, **kwargs):
+        if request.body:
+            data = json.loads(request.body)
+            form = CreateScrapper(data)
+            if form.is_valid():
+                scraper = form.save()
+                return JsonResponse({
+                    'id': scraper.id,
+                    'created_at': scraper.created_at,
+                    'currency': scraper.currency,
+                    'frequency': scraper.frequency
+                }, safe=False)
 
-    def put(self, *args, **kwargs):
-        pass
+        return JsonResponse({
+            'error': json.loads(form.errors.as_json())
+        }, status=400, content_type="application/json")
 
-    def delete(self, *args, **kwargs):
-        pass
+    def put(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        form = UpdateScrapper(data)
+        if form.is_valid():
+            scraper = form.save()
+            return JsonResponse({
+                "msg": "The scraper with id {} was updated successfully".format(scraper.id)
+            }, safe=False)
+        else:
+            return JsonResponse({
+                'error': ', '.join(['{}: {}'.format(k, v[0]) for k, v in form.errors.items()])
+            }, safe=False, status=400)
+
+    def delete(self, request, *args, **kwargs):
+        data = json.loads(request.body)
+        form = DeleteScrapper(data)
+        if form.is_valid():
+            scraper = form.delete()
+            return JsonResponse({
+                "msg": "The scraper with id {} was deleted successfully".format(data['id'])
+            }, safe=False)
+        else:
+            return JsonResponse({
+                'error': ', '.join(['{}: {}'.format(k, v[0]) for k, v in form.errors.items()])
+            }, safe=False, status=400)
