739eeb4626c571898350d052020c7d1e93d22e37
Erik Sanchez <erik.sape@gmail.com>
2020-09-06 20:49:36 -0500

    :sparkles: added a model django admin

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import Scraper
+
+
+@admin.register(Scraper)
+class ScraperAdmin(admin.ModelAdmin):
+    list_display = (
+        'currency',
+        'frequency',
+        'value',
+        'created_at',
+        'value_updated_at',
+    )
+    ordering = ('currency', 'frequency', 'value', 'created_at', 'value_updated_at',)
+    readonly_fields = ('created_at', 'value_updated_at',)
+    search_fields = ('currency', 'frequency', 'value',)
