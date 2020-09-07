121a6774a48d74dcc30f7ecbe35a35cc7d2be229
Erik Sanchez <erik.sape@gmail.com>
2020-09-07 10:38:05 -0500

    :recycle: can set value

--- api/forms.py
+++ api/forms.py
@@ -15,6 +15,7 @@ class CreateScrapper(forms.ModelForm):
 
     currency = forms.CharField(min_length=1, max_length=50, required=True)
     frequency = forms.IntegerField(required=True)
+    value = forms.IntegerField(required=False)
 
     def clean_currency(self):
         currency = self.cleaned_data['currency']
@@ -26,6 +27,8 @@ class CreateScrapper(forms.ModelForm):
     def save(self):
         """Create Scraper from data."""
         scraper = Scraper(currency=self.cleaned_data['currency'], frequency=self.cleaned_data['frequency'])
+        if self.cleaned_data['value']:
+            scraper.value = self.cleaned_data['value']
         scraper.save()
         return scraper
 
@@ -42,6 +45,7 @@ class UpdateScrapper(forms.ModelForm):
 
     id = forms.IntegerField(required=True)
     frequency = forms.IntegerField(required=True)
+    value = forms.IntegerField(required=False)
 
     def clean_id(self):
         id_exists = Scraper.objects.filter(id=self.cleaned_data['id']).exists()
@@ -53,6 +57,8 @@ class UpdateScrapper(forms.ModelForm):
         """Updated Scraper from data."""
         scraper = Scraper.objects.filter(id=self.cleaned_data['id']).get()
         scraper.frequency = self.cleaned_data['frequency']
+        if self.cleaned_data['value']:
+            scraper.value = self.cleaned_data['value']
         scraper.save()
         return scraper
 
